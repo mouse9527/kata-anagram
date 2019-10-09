d0ff59cc7ff218c8dcefe62e096be46dc8755900
mouse <m1689141525@gmail.com>
2019-10-09 23:21:44 +0800

    puzzle

--- src/main/java/com/mouse/Interpreter.java
+++ src/main/java/com/mouse/Interpreter.java
@@ -3,6 +3,7 @@ package com.mouse;
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.io.IOException;
 import java.net.URL;
@@ -12,9 +13,14 @@ import static com.google.common.base.Charsets.UTF_8;
 
 class Interpreter {
     @SuppressWarnings("UnstableApiUsage")
-    List<String> readWordList(String resourceName) throws IOException {
+    List<String> readWordList(String resourceName) {
         URL resource = Resources.getResource(resourceName);
-        List<String> lines = Resources.readLines(resource, UTF_8);
+        List<String> lines;
+        try {
+            lines = Resources.readLines(resource, UTF_8);
+        } catch (IOException e) {
+            throw new IllegalStateException("读取文件出错！");
+        }
         List<String> result = Lists.newArrayList();
         lines.forEach(x -> {
             String[] split = x.split(" ");
@@ -25,6 +31,23 @@ class Interpreter {
                 result.add(word);
             }
         });
+        return result;
+    }
+
+    List<Pair<String, String>> puzzle(List<String> wordList) {
+        Anagram anagram = new Anagram();
+        List<String> filteredWordList = anagram.filter(wordList);
+        List<Pair<String, String>> result = Lists.newArrayList();
+        int size = filteredWordList.size();
+        for (int i = 0; i < size; i++) {
+            for (int y = i + 1; y < size; y++) {
+                String word1 = filteredWordList.get(i);
+                String word2 = filteredWordList.get(y);
+                if (anagram.match(word1, word2)) {
+                    result.add(Pair.of(word1, word2));
+                }
+            }
+        }
         return result;
     }
 }
--- src/test/java/com/mouse/InterpreterTest.java
+++ src/test/java/com/mouse/InterpreterTest.java
@@ -1,8 +1,10 @@
 package com.mouse;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -11,8 +13,22 @@ import static org.junit.Assert.assertThat;
 public class InterpreterTest {
 
     @Test
-    public void should_read_word_list_from_file_correctly() throws IOException {
+    public void should_read_word_list_from_file_correctly() {
         assertThat(new Interpreter().readWordList("wordlist.txt").size(), is(1633));
     }
 
+    @Test
+    public void should_puzzle_correctly() {
+        assertThat(new Interpreter().puzzle(getWordList()), is(getExpected()));
+    }
+
+    private ArrayList<Pair<String, String>> getExpected() {
+        return Lists.newArrayList(Pair.of("uetnno", "cdmgi"),
+                Pair.of("uetnno", "cdmig"),
+                Pair.of("uetnno", "dcmgi"));
+    }
+
+    private List<String> getWordList() {
+        return Lists.newArrayList("uetnno", "cdmgi", "cdmig", "uetno", "uetnnoo", "dcmgi");
+    }
 }
